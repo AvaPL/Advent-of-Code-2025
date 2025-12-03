5e8c110b2ca8e59a237a940be2cdd938efda0d3d
Paweł Cembaluk <pawel.cembaluk@gmail.com>
2025-12-03 09:24:02 +0100

    Simplify day 3 solution.

--- src/main/scala/day3/package.scala
+++ src/main/scala/day3/package.scala
@@ -5,16 +5,17 @@ case class Battery(joltage: Long)
 
 case class BatteryBank(batteries: Vector[Battery])
 
-def maxJoltageBatteries(batteryCount: Int)(batteryBank: BatteryBank): Vector[Battery] = 
+def maxJoltageBatteries(batteryCount: Int)(batteryBank: BatteryBank): Vector[Battery] =
   if (batteryCount <= 0) Vector.empty
   else {
-    val battery1Index = maxBatteryIndex(batteryBank)
+    val selectedBatteryIndex = maxBatteryIndex(batteryBank)
     val newBatteryCount = batteryCount - 1
-    val rightSubBank = BatteryBank(batteryBank.batteries.drop(battery1Index + 1))
-    val rightBatteryCount = math.min(batteryBank.batteries.size - (battery1Index + 1), newBatteryCount)
-    val leftSubBank = BatteryBank(batteryBank.batteries.take(battery1Index))
+    val rightSubBank = BatteryBank(batteryBank.batteries.drop(selectedBatteryIndex + 1))
+    val rightBatteryCount = math.min(batteryBank.batteries.size - (selectedBatteryIndex + 1), newBatteryCount)
+    val leftSubBank = BatteryBank(batteryBank.batteries.take(selectedBatteryIndex))
     val leftBatteryCount = newBatteryCount - rightBatteryCount
-    maxJoltageBatteries(leftBatteryCount)(leftSubBank) :++ batteryBank.batteries(battery1Index) +: maxJoltageBatteries(rightBatteryCount)(rightSubBank)
+
+    maxJoltageBatteries(leftBatteryCount)(leftSubBank) :++ batteryBank.batteries(selectedBatteryIndex) +: maxJoltageBatteries(rightBatteryCount)(rightSubBank)
   }
 
 private def maxBatteryIndex(batteryBank: BatteryBank) = {
